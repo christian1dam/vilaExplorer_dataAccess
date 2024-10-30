4e7f5406f79b1f6ccaed3b592013376df86bc6ea
Christian Mamani <christianmamani.dam@gmail.com>
2024-10-30 10:19:32 +0100

    auth development removed

--- src/main/java/app/VilaExplorer/controller/AdministradorController.java
+++ src/main/java/app/VilaExplorer/controller/AdministradorController.java
@@ -5,12 +5,13 @@ import app.VilaExplorer.domain.Administrador;
 import app.VilaExplorer.service.AdministradorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/api/administradores")
 public class AdministradorController {
 
--- src/main/java/app/VilaExplorer/controller/PlatoController.java
+++ src/main/java/app/VilaExplorer/controller/PlatoController.java
@@ -4,12 +4,13 @@ import app.VilaExplorer.domain.Plato;
 import app.VilaExplorer.service.PlatoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/api/platos")
 public class PlatoController {
 

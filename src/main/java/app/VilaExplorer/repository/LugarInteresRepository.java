31aa660e87e31837eeb26df71699f339e0c47b0f
Ranger <aangelcruzmx@gmail.com>
2024-10-31 13:27:45 +0100

    Correciones y mejoras en las cases relacionadas con LugarInteres

--- src/main/java/app/VilaExplorer/repository/LugarInteresRepository.java
+++ src/main/java/app/VilaExplorer/repository/LugarInteresRepository.java
@@ -3,7 +3,10 @@ package app.VilaExplorer.repository;
 import app.VilaExplorer.domain.LugarInteres;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface LugarInteresRepository extends JpaRepository<LugarInteres, Long> {
+
+    List<LugarInteres> findAllByActivoTrue();
 }
--- src/main/java/app/VilaExplorer/repository/PuntuacionLugarRepository.java
+++ src/main/java/app/VilaExplorer/repository/PuntuacionLugarRepository.java
@@ -1,9 +1,15 @@
 package app.VilaExplorer.repository;
 
+import app.VilaExplorer.domain.LugarInteres;
 import app.VilaExplorer.domain.PuntuacionLugar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PuntuacionLugarRepository extends JpaRepository<PuntuacionLugar, Long> {
+
+
+
 }
